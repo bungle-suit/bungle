e09eff199822618a1cdab5119a827b3d2382b25b
Red Forks <redforks@gmail.com>
2020-03-31 08:48:25 +0800

    ErrorCodeMessage trait

---
+++ DingTalk/src/Hydrate/ErrorCodeMessage.php
@@ -0,0 +1,36 @@
+<?php
+declare(strict_types=1);
+
+namespace Bungle\DingTalk\Hydrate;
+
+use RuntimeException;
+use Symfony\Component\Serializer\Annotation\SerializedName;
+
+trait ErrorCodeMessage
+{
+    // success if 0, error if other value.
+    /** @SerializedName("errcode") */
+    public int $errorCode = 0;
+    /** @SerializedName("errmsg") */
+    public string $errorMessage = '';
+
+    /**
+     * Check errcode raise exception if errcode not 0.
+     */
+    public function checkCode(): void
+    {
+        $errCode = $this->errorCode;
+        $errMsg = $this->errorMessage;
+        if (0 !== $errCode) {
+            throw new RuntimeException("[DingTalk] $errCode/$errMsg");
+        }
+    }
+
+    public static function hydrate(array $ddResponse): self
+    {
+        /** @var self $r */
+        $r = HydrateUtil::hydrate($ddResponse, self::class);
+
+        return $r;
+    }
+}
